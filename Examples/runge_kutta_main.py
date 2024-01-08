5d16116f948898b019252d23fbbb49fca8b9e617
Viktor Filimonov <tf349@mail.ru>
2024-01-08 12:37:10 +0300

    added coloring for fixed circles in runge_kutta_main.py

--- Examples/runge_kutta_main.py
+++ Examples/runge_kutta_main.py
@@ -1,6 +1,5 @@
 import time
 
-import numpy as np
 import numpy.random
 from matplotlib import pyplot as plt
 from numpy.random import randint
@@ -18,10 +17,11 @@ P = polygons_dict['P3']
 t0 = time.perf_counter()
 
 numpy.random.seed(42)
-centers = [Point(randint(-5, 5), randint(-5, 5)) for i in range(30)]
+centers = [Point(randint(-5, 5), randint(-5, 5)) for i in range(10)]
 alg = RungeKuttaAlgorithm(centers, 1.5)  # Укажем данные.
 alg.set_params(
-    #fixed=[0, 1, 1, 1, 0, 0, 0, 0, 0, 0]  # TODO: покрасить цветом, чтобы было видно на графике, что не двигалось
+    fixed=[0, 1, 1, 1, 0, 0, 0, 0, 0, 0]
+    # fixed=[0, 1, 1, 1, 0, 0, 0, 0, 0, 0]
 )  # Укажем параметры решения.
 # Запуск алгоритма
 t1 = time.perf_counter()
@@ -40,12 +40,26 @@ fig, ax = plt.subplots(nrows=1, ncols=2)
 # Отрисуем результат до запуска внутреннего алгоритма.
 ax[0].set_aspect('equal', adjustable='box')
 draw_polygon(ax[0], P)
-draw_circles(ax[0], [Circle(p, 1.5) for p in centers]) # TODO: отрисовывать, что было до
+
+# Нарисуем разными цветами зафиксированные и незафиксированные круги.
+draw_circles(ax[0],
+             [Circle(centers[i], 1.5) for i in range(len(alg.fixed)) if
+              not alg.fixed[i]])  # TODO: отрисовывать, что было до
+
+draw_circles(ax[0],
+             [Circle(centers[i], 1.5) for i in range(len(alg.fixed)) if alg.fixed[i]],
+             color='red',
+             center_color='darkred')
 
 # Отрисуем результат после запуска внутреннего алгоритма.
-
 ax[1].set_aspect('equal', adjustable='box')
 draw_polygon(ax[1], P)
-draw_circles(ax[1], ans)
+# Нарисуем разными цветами зафиксированные и незафиксированные круги.
+draw_circles(ax[1],
+             [ans[i] for i in range(len(alg.fixed)) if not alg.fixed[i]])
+draw_circles(ax[1],
+             [ans[i] for i in range(len(alg.fixed)) if alg.fixed[i]],
+             color='red',
+             center_color='darkred')
 
 plt.show()
